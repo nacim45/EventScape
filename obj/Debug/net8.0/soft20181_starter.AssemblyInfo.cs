8d89f8fef72ef57326f2955545338f1e515b68c5
nacim45 <nemmouchenacim@hotmail.com>
2025-08-04 22:13:19 +0100

    crud op fix 12

--- obj/Debug/net8.0/soft20181_starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/soft20181_starter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("soft20181_starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8e884015bfb175df5d63be5d4a85d2451d4b41b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fae867ebf89f2eb8774ce9c09016983a9e826e40")]
 [assembly: System.Reflection.AssemblyProductAttribute("soft20181_starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("soft20181_starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
